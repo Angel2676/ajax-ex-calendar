418e0100b24ddce3cc5d68323851820f3d9f6e6c
Angelo <angelo_1976@icloudcom>
2020-09-01 22:10:07 +0200

    riper

--- app/script.js
+++ app/script.js
@@ -1,99 +1,114 @@
-// https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
+//-------API-----/
+// Link API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
+// *******Descrizione***********
+// Creiamo un calendario dinamico con le festività.
+// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
+// *******Milestone 1********
+// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+// *******Milestone 2********
+// Diamo la possibilità di cambiare mese, gestendo il caso in cui l’API non possa ritornare festività.
+// ********Attenzione!******************
+// Ogni volta che cambio mese dovrò:
+// Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018)
+// Controllare quanti giorni ha il mese scelto formando così una lista
+// Chiedere all’API quali sono le festività per il mese scelto
+// Evidenziare le festività nella lista.
 
-
+//********SVOLGIMENTO***********
 $(document).ready(function(){
 
+    // var dataDiPartenza = moment("2018-01-01");
+    var dataDiPartenza = moment($('h1#mese').attr('data-partenza')); // MEMORIZZO l'attributo data-partenza del tag h1 e lo considero come data di partenza per generare le date
 
-$("#next").click(function(){
-    var newDate = moment().add(1,"months");
-    var dataT = $("h1").attr("data-this-date", newDate);
-    insertDays(dataT);
-    insertHolidays(dataT);
-    console.log(dataT);
+    inserisciDate(dataDiPartenza);
 
-})
+    inserisciFesta(dataDiPartenza);
 
+    $('button#next').click(function(){
+        next(dataDiPartenza);
+    })
 
+    $('button#prev').click(function(){
+        prev(dataDiPartenza);
+    })
 
-
-
-
-// data di partenza
-// creaiamo un oggetto moment su data di partenza
-var dataCorrente = moment("2018-01-01");
-// console.log(dataCorrente);
-insertDays(dataCorrente);
-insertHolidays(dataCorrente);
-
-// console.log(dataCorrente);
-// 2 step - Andare a scrivere il nostro mese e anno in h1
-
-
-
-
-
-
-// Fine document
 });
-// ***********FUNZIONI*********
 
-function insertDays(data){
-    // 2 step - Andare a scrivere il nostro mese e anno in h1
-    var month = data.format("MMMM");
-    var year = data.format("YYYY");
-    $("h1.month").html(month+ " "+ year);
-    // 3 Step - generare i giorni
-    // *****Calcolare il 31 con moment*****
-    var daysMonth = data.daysInMonth();
-    // console.log(daysMonth);
-    for (var i = 1; i <= daysMonth; i++) {
-        var source = $("#day-template").html();
+// --- funzioni --- //
+
+function addZero(n){
+    if (n < 10){
+        return '0' + n;
+    }
+    return n;
+}
+
+function inserisciDate(data){
+    $('ul#calendario').empty();                         // svuoto l'elenco ul prima di riempirlo
+
+    var giorniTotali = data.daysInMonth();              // calcolo i giorni totali in un mese
+
+    var meseParola = data.format('MMMM');       // memorizzo in una variabile il nome del mese
+    var anno = data.year();                     // memorizzo in una variabile l'anno
+
+    $('h1#mese').html(meseParola + ' ' + anno);         // implemento l'h1 con mese e anno
+
+    for (var i = 1; i <= giorniTotali; i++){            // ciclo tutti i giorni del mese
+        var source = $("#entry-template").html();
         var template = Handlebars.compile(source);
 
         var context = {
-            "day" : addZero(i),
-            "month" : month,
-            "completeDate" : year + "-" + data.format("MM") + "-" + addZero(i)
+            giorno: addZero(i),
+            mese: meseParola,
+            dataCompleta: anno + '-' + data.format('MM') + '-' + addZero(i)
         };
         var html = template(context);
 
-    $(".month-list").append(html)
-
-
+        $('#calendario').append(html);
     }
+}
 
-
-};
-
-function addZero(n){
-    if (n<10) {
-        return "0"+n
-
-    } return n
-
-};
-
-function insertHolidays(data) {
+function inserisciFesta(data){
     $.ajax(
         {
-            url:'https://flynn.boolean.careers/exercises/api/holidays',
+            url: 'https://flynn.boolean.careers/exercises/api/holidays',
             method:'GET',
-            data: {
-                year:data.year(),
-                month:data.month()
+            data:{
+                year: data.year(),
+                month: data.month()
             },
             success: function(risposta){
-                // console.log(risposta.response);
-                for (var i = 0; i < risposta.response.length; i++) {
-                    var listItem = $('li[data-complete-date="'+ risposta.response[i].date + '"]');
-                    listItem.append("-" + risposta.response[i].name);
-                    listItem.addClass("holiday")
+                for (var i = 0; i < risposta.response.length; i++){
+                    var elemento = $('li[data-completa="' + risposta.response[i].date + '"]');
+                    elemento.addClass("festa");
+                    elemento.append(' - ' + risposta.response[i].name);
+                    console.log(elemento);
                 }
 
             },
             error: function(){
-                alert("errore")
+                alert('Si è verificato un errore');
             }
         }
     );
+}
+
+function next(data){
+    if (data.month() == 11){
+        alert('Non puoi proseguire');
+    } else {
+        data.add(1, 'months');
+        inserisciDate(data);
+        inserisciFesta(data);
+    }
+}
+
+function prev(data){
+    if (data.month() == 0){
+        alert('Non puoi proseguire');
+    } else {
+        data.subtract(1, 'months');
+        inserisciDate(data);
+        inserisciFesta(data);
+    }
 }
